67c82e0d8268a55c7481b12443fa8c0929ece200
Jefferson <luis.88.rasini@gmail.com>
2019-04-30 10:42:02 -0300

    1º commit + basico_dart + oper_aritmeticas + oper_logicas

---
+++ lib/basico_dart.dart
@@ -0,0 +1,15 @@
+void main() {
+  String nome = "Loja do Jeff";
+  int banana = 5;
+  double preco = 12.50;
+  bool aindaTem = true;
+
+  print("O nome da loja é: " + nome);
+  print("A $nome tem $banana bananas");
+
+  dynamic teste = 1;
+  print(teste);
+  teste = "oi";
+  print(teste);
+
+}
---
+++ lib/operacoes_aritmeticas.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  double num1 = 10.0;
+  double num2 = 20.0;
+
+  //double result = num1 + num2;
+
+  //double result = (num1 * num2) - 30.0;
+
+  //num1 = num1 + 15;
+
+  num1 += 15;
+  num1++;
+
+  print(num1);
+
+}
